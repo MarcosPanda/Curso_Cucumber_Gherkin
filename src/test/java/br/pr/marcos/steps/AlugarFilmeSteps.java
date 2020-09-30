6b12a1d601d70abf1e33cbfbf350558b491e0af8
MarcosPanda <marcosleandronascimento@hotmail.com>
2020-09-30 00:56:06 -0300

    Aula 20

--- src/test/java/br/pr/marcos/steps/AlugarFilmeSteps.java
+++ src/test/java/br/pr/marcos/steps/AlugarFilmeSteps.java
@@ -14,54 +14,59 @@ import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class AlugarFilmeSteps {
-	
+
 	private Filme filme;
 	private AluguelService aluguel = new AluguelService();
 	private NotaAluguel nota;
-	
+	private String erro;
+
 	@Dado("^um filme com estoque de (\\d+) unidades$")
 	public void umFilmeComEstoqueDeUnidades(int arg1) throws Throwable {
-	    filme = new Filme();
-	    filme.setEstoque(arg1);
+		filme = new Filme();
+		filme.setEstoque(arg1);
 	}
 
 	@Dado("^que o preço do aluguel seja R\\$ (\\d+)$")
 	public void queOPreçoDoAluguelSejaR$(int arg1) throws Throwable {
-	    filme.setAluguel(arg1);
+		filme.setAluguel(arg1);
 	}
 
 	@Quando("^alugar$")
 	public void alugar() throws Throwable {
-	    nota = aluguel.alugar(filme);
+		try {
+			nota = aluguel.alugar(filme);
+		} catch (RuntimeException e) {
+			erro = e.getMessage();
+		}
+	    
 	}
 
 	@Então("^o preço do aluguel será R\\$ (\\d+)$")
 	public void oPreçoDoAluguelSeráR$(int arg1) throws Throwable {
-	    Assert.assertEquals(arg1, nota.getPreco());
+		Assert.assertEquals(arg1, nota.getPreco());
 	}
 
 	@Então("^a data de entrega será no dia seguinte$")
 	public void aDataDeEntregaSeráNoDiaSeguinte() throws Throwable {
-	    Calendar cal = Calendar.getInstance();
-	    cal.add(Calendar.DAY_OF_MONTH, 1);
-	    
-	    Date dataRetorno = nota.getDataEntrega();
-	    Calendar calRetorno = Calendar.getInstance();
-	    calRetorno.setTime(dataRetorno);
-	    
-	    Assert.assertEquals(cal.get(Calendar.DAY_OF_MONTH), calRetorno.get(Calendar.DAY_OF_MONTH));
-	    Assert.assertEquals(cal.get(Calendar.MONTH), calRetorno.get(Calendar.MONTH));
-	    Assert.assertEquals(cal.get(Calendar.YEAR), calRetorno.get(Calendar.YEAR));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+
+		Date dataRetorno = nota.getDataEntrega();
+		Calendar calRetorno = Calendar.getInstance();
+		calRetorno.setTime(dataRetorno);
+
+		Assert.assertEquals(cal.get(Calendar.DAY_OF_MONTH), calRetorno.get(Calendar.DAY_OF_MONTH));
+		Assert.assertEquals(cal.get(Calendar.MONTH), calRetorno.get(Calendar.MONTH));
+		Assert.assertEquals(cal.get(Calendar.YEAR), calRetorno.get(Calendar.YEAR));
 	}
 
 	@Então("^o estoque do filme será (\\d+) unidade$")
 	public void oEstoqueDoFilmeSeráUnidade(int arg1) throws Throwable {
-	    Assert.assertEquals(arg1, filme.getEstoque());
+		Assert.assertEquals(arg1, filme.getEstoque());
 	}
-	
+
 	@Então("^não será possível por falta de estoque$")
 	public void nãoSeráPossívelPorFaltaDeEstoque() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		Assert.assertEquals("Filme sem estoque", erro);
 	}
 }
